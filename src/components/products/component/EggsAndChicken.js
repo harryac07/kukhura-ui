6f934508e73547831f692857e76e25e20c5ca670
hari adhikari <hari@vainu.io>
2019-12-29 22:46:06 +0200

    show more product button for displaying 3 product at a time

--- src/components/products/component/EggsAndChicken.js
+++ src/components/products/component/EggsAndChicken.js
@@ -1,30 +1,51 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { truncate } from 'lodash'
+import { truncate, take } from 'lodash'
 import ComponentWrapper from 'components/common/components/ComponentWrapper'
 import Title from 'components/common/components/Title'
 import ImageTitleText from 'components/common/components/ImageTitleTextButton'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
+import { OrangeButton } from 'components/common/components/Button'
+
 const useStyles = makeStyles(theme => ({
     maindiv: {
         padding: '100px 0px',
         width: '100%',
         background: '#f0f0f0',
+    },
+    centerDiv: {
+        width: '100%',
+        display: 'block',
+        textAlign: 'center'
+    },
+    readMoreButton: {
+        width: '40%',
+        borderRadius: '15px',
+        margin: '0px 0px 0px 0px'
     }
 }));
 
 const EggsAndChiken = (props) => {
     const classes = useStyles();
     const [showMoreProduct, onShowMoreProductChange] = useState(false);
+    const [loadMoreProducts, onLoadMoreProductsChange] = useState(3);
+
     const { allProducts } = props
+    const allProductsWithoutHeroProduct = allProducts.filter(each => !each.hero_product);
+    const filteredAndTruncatedProducts = take(
+        allProductsWithoutHeroProduct,
+        loadMoreProducts
+    )
+    const allProductsShown = loadMoreProducts >= allProductsWithoutHeroProduct.length
     return (
         <ComponentWrapper backgroundColor="#f0f0f0">
             <Title text={"Organic Products"} padding="0px 0px 24px 0px" />
             <Grid container spacing={3}>
+                {/* display all Products */}
                 {
-                    allProducts
+                    filteredAndTruncatedProducts
                         .filter(each => !each.hero_product)
                         .map(each => {
                             const {
@@ -40,7 +61,7 @@ const EggsAndChiken = (props) => {
                                         title={title.toUpperCase()}
                                         text={
                                             truncate(description || "", {
-                                                'length': showMoreProduct ? description.length : 100,
+                                                'length': showMoreProduct ? description.length : 200,
                                             })
                                         }
                                         textBgColor={"inherit"}
@@ -48,6 +69,27 @@ const EggsAndChiken = (props) => {
                                 </Grid>
                             )
                         })
+                }
+
+                {/* Load more button  */}
+                {
+                    allProductsShown
+                        ? null
+                        : <Grid item xs={12} sm={12}>
+                            <div className={classes.centerDiv}>
+                                <OrangeButton
+                                    onClick={() =>
+                                        loadMoreProducts < allProductsWithoutHeroProduct.length
+                                            ? onLoadMoreProductsChange(loadMoreProducts + 3)
+                                            : onLoadMoreProductsChange(allProductsWithoutHeroProduct.length)
+                                    }
+                                    padding="12px"
+                                    className={classes.readMoreButton}
+                                >
+                                    Show More
+                                </OrangeButton>
+                            </div>
+                        </Grid>
                 }
             </Grid>
         </ComponentWrapper>
